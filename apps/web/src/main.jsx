e7498d528e4b52a9fe9644dd2d5d03f6b0bf2e5f
proofofputt <pop@proofofputt.com>
2025-08-29 21:28:48 -0400

    Serve React app at root

--- apps/web/src/main.jsx
+++ apps/web/src/main.jsx
@@ -9,7 +9,7 @@ import App from './App.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/app">
+    <BrowserRouter>
       <NotificationProvider>
         <AuthProvider>
           <App />
