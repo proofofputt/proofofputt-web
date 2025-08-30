cab63659d13042af8ad8d701b3e967ad838441fb
proofofputt <pop@proofofputt.com>
2025-08-30 00:00:48 -0400

    Consolidate backend

--- api/index.py
+++ api/index.py
@@ -1,8 +1,4 @@
-import sys
-import os
-sys.path.append(os.path.join(os.path.dirname(__file__), '..', 'apps', 'api'))
-
-from api import app
+from .api import app
 
 def handler(request):
     return app(request.environ, lambda status, headers: None)
