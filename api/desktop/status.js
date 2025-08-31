35b8b97034e1ac5f1dd15b33cd736b0bef70c6eb
proofofputt <pop@proofofputt.com>
2025-08-31 00:38:24 -0400

    latest

--- api/desktop/status.js
+++ api/desktop/status.js
@@ -1,5 +1,6 @@
 export default function handler(req, res) {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
   
